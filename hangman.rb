5e6287440ca429a15706529ecfa37045127ba943
sirussalari <sirus.salari@gmail.com>
2022-05-22 00:04:58 -0700

    Add functionality to save/load games
    In this new version, players can now choose to save their game at any point in the game. When the program is first started, the program prompts for your name and checks to see if you already have a saved game, and asks if you'd like to load a saved game or start a new one. Finally, if a player already has 1 saved game and wants to save again, the program gives the option of either overwriting a saved game, or creating a new saved game.

--- hangman.rb
+++ hangman.rb
@@ -1,10 +1,22 @@
 class Player
     @@incorrect_letters = []
+    @@instance = nil
 
-    def initialize(name, version)
+    attr_reader :name
+
+    def initialize(name, version = nil)
+        @@instance = self
         @name = name
         @version = version
-        Computer.start_game(version)
+
+        if version != nil
+            Computer.start_game(version)
+
+        else
+            Display.count
+            Display.incorrect_letters
+        end
+
         Display.correct_letters
         Computer.prompt_guess
     end
@@ -12,16 +24,25 @@ class Player
     def self.incorrect_letters
         return @@incorrect_letters
     end
+
+    def self.load_incorrect_letters(incorrect_letters)
+        @@incorrect_letters = incorrect_letters
+    end
+
+    def self.instance
+        return @@instance
+    end
 end
 
 class Computer
     @@word = nil
 
-    def initialize
-    end
-
     def self.word
         return @@word
+    end
+
+    def self.load_word(word)
+        @@word = word
     end
 
     def self.start_game(version)
@@ -52,11 +73,15 @@ class Computer
     end
 
     def self.prompt_guess
-        puts "Guess a letter or word"
+        puts "To save the game, type 'save'\nGuess a letter or word"
         guess = gets.chomp
         guess.downcase!
 
-        Computer.guess_correct?(guess)
+        if guess == "save"
+            Computer.save_game
+        else
+            Computer.guess_correct?(guess)
+        end
     end
 
     def self.guess_correct?(letter)
@@ -107,6 +132,61 @@ class Computer
             Computer.prompt_guess
         end
     end
+
+    def self.save_game
+        if !Dir.exist?('saved-games')
+            Dir.mkdir('saved-games')
+        end
+
+        Data.add_data(:name, Player.instance.name)
+        Data.add_data(:hidden, Display.hidden)
+        Data.add_data(:incorrect_letters, Player.incorrect_letters)
+        Data.add_data(:word, Computer.word)
+
+        serialized_data = Marshal.dump(Data.saved_data)
+
+        if File.file?("saved-games/#{Player.instance.name}1.dump")
+            puts "It looks like you already have a saved game, #{Player.instance.name}"
+            puts "Would you like to overwrite your saved game (type 'overwrite') or create a new saved game (type 'new')?"
+
+            response = gets.chomp
+            
+            if response == 'overwrite'
+                puts 'Which iteration would you like to overwrite?'
+                iteration =  gets.chomp
+
+                File.delete("saved-games/#{Player.instance.name}#{iteration}.dump")
+
+                file = File.open("saved-games/#{Player.instance.name}#{iteration}.dump", 'wb')
+
+                file.write(serialized_data)
+
+                file.close
+
+            elsif response == 'new'
+                iteration = 2
+
+                while File.file?("saved-games/#{Player.instance.name}#{iteration}.dump")
+                    iteration += 1
+                end
+
+                file = File.open("saved-games/#{Player.instance.name}#{iteration}.dump", 'wb')
+                file.write(serialized_data)
+                file.close
+
+                puts "Your game has been saved with an iteration value of #{iteration}"
+            end
+        else
+
+            file = File.open("saved-games/#{Player.instance.name}1.dump", 'wb')
+
+            file.write(serialized_data)
+
+            file.close
+
+            puts "Your game has been saved with an iteration value of 1"
+        end
+    end
 end
 
 class Display
@@ -118,6 +198,14 @@ class Display
 
     def self.hidden
         return @@hidden
+    end
+
+    def self.load_hidden(hidden)
+        @@hidden = hidden
+    end
+
+    def self.load_guess_number(guess_number)
+        @@guess_number = guess_number
     end
 
     def self.count(incorrect = false)
@@ -173,13 +261,57 @@ class Display
     end
 end
 
-#puts "Hello! What is your name?"
-#name = gets.chomp
+class Data
+    @@saved_data = {}
 
-#puts "Hey #{name}! Would you like to play a clean version or NSFW version?(type 'clean' or 'nsfw')"
-#version = gets.chomp
+    def self.saved_data
+        return @@saved_data
+    end
 
-version = 'clean'
-name = 'sirus'
+    def self.add_data(key, value)
+        Data.saved_data[key] = value
+    end
+end
 
-player = Player.new(name, version)
+puts "Hello! What is your name?"
+name = gets.chomp
+
+if File.exist?("saved-games/#{name}1.dump")
+    puts "It looks like you already have at least one saved game"
+    puts "Would you like to load a saved game (type 'load') or start a new game (type 'new')?"
+
+    response = gets.chomp
+
+    if response == 'load'
+        deserialized_data = nil
+
+        if File.exist?("saved-games/#{name}2.dump")
+            puts "Which saved game would you like to open (type the assigned iteration number)"
+            iteration = gets.chomp
+    
+            deserialized_data = Marshal.load(File.open("saved-games/#{name}#{iteration}.dump"))
+    
+        else
+            deserialized_data = Marshal.load(File.open("saved-games/#{name}1.dump"))
+        end
+    
+        Computer.load_word(deserialized_data[:word])
+        Player.load_incorrect_letters(deserialized_data[:incorrect_letters])
+        Display.load_hidden(deserialized_data[:hidden])
+        Display.load_guess_number(deserialized_data[:incorrect_letters].length)
+        player = Player.new(name)
+
+    else
+        puts "Would you like to play a clean version or NSFW version?(type 'clean' or 'nsfw')"
+        version = gets.chomp
+
+        player = Player.new(name, version)
+        
+    end
+
+else
+    puts "Would you like to play a clean version or NSFW version?(type 'clean' or 'nsfw')"
+    version = gets.chomp
+
+    player = Player.new(name, version)
+end
